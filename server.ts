0e8c881709191ceda37222aa8590ecc17b9b69d4
Jefri Herdi Triyanto <jefripunza@gmail.com>
2024-05-19 03:32:47 +0700

    save

--- server.ts
+++ server.ts
@@ -45,7 +45,7 @@ Deno.serve({ port: 8080 }, async (request) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Live Location Viewer</title>
+    <title>OME.TV | Live Location Viewer</title>
     <style>
       table {
         font-family: arial, sans-serif;
